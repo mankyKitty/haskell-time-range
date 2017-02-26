c2d63a80a98b373aed29512cac73be686bf6b8ba
Sean Chalmers <sean.chalmers@iseek.com.au>
2017-02-27 08:48:05 +1000

    Actual things that look like and smell like Docs and Tests. Sort of.

--- src/Data/Time/Range.hs
+++ src/Data/Time/Range.hs
@@ -15,7 +15,7 @@ import           Control.Category      ((.))
 import           Control.Lens          (Choice, Optic', Prism', Rewrapped,
                                         Unwrapped, from, makeClassy, makeLenses,
                                         makeWrapped, prism', to, (^.), _1, _2,
-                                        _3, _Wrapped)
+                                        _3, _Wrapped, (#))
 
 import           GHC.Generics          (Generic)
 import           GHC.Natural           (Natural)
@@ -99,7 +99,20 @@ dayParse s = maybe err Right $
     nom f = parseTimeM True defaultTimeLocale f s
     err = Left $ "Unable to parse Date Accepts [yyyy-mm-dd, yyyymmdd, mm/dd/yy, dd/mm/yyyy]: " <> s
 
-bounds
+-- | Given a wrapped start and end @Day@, as well as a chosen @Hour@, create a pair of wrapped @UTCTime@
+-- values that represent their upper and lower values. Used internally.
+-- $setup
+-- >>> import Control.Lens
+-- >>> import Data.Time
+-- >>> :set -XFlexibleContexts
+-- >>> let startD = _Wrapped # (fromGregorian (2017 :: Integer) (2 :: Int) (25 :: Int)) :: StartDate
+-- >>> let endD = _Wrapped # (fromGregorian (2017 :: Integer) (2 :: Int) (27 :: Int)) :: EndDate
+-- >>> let startH = (_Wrapped # 3) :: StartHour
+-- >>> let endH = (_Wrapped # 4) :: EndHour
+
+-- >>> bounds startD endD startH endH
+-- LowerBound 2017-02-25 03:00:00 UTC,UpperBound 2017-02-27 04:00:00 UTC)
+boundFromWrapped
   :: ( Rewrapped s s
      , Rewrapped e e
      , Unwrapped s ~ Day
@@ -114,7 +127,7 @@ bounds
   -> hs
   -> he
   -> (LowerTimeBound, UpperTimeBound)
-bounds sd ed sh eh =
+boundFromWrapped sd ed sh eh =
   ( (toUtc sd sh) ^. from _Wrapped
   , (toUtc ed eh) ^. from _Wrapped
   )
@@ -134,6 +147,19 @@ bounds sd ed sh eh =
       $ h ^. _Wrapped
       )
 
+-- | Provide a range builder for any wrapped types that have
+-- an @Ord@ instance.
+-- $setup
+-- >>> import Control.Lens
+-- >>> import Data.Time
+-- >>> import Data.List.NonEmpty (NonEmpty)
+-- >>> let startH = (_Wrapped # 3) :: StartHour
+-- >>> let endH = (_Wrapped # 5) :: EndHour
+
+-- >>> wrappedRange (_Wrapped #) succ startH endH :: NonEmpty StartHour
+-- StartHour 3 :| [StartHour 4, StartHour 5]
+-- >>> wrappedRange (_Wrapped #) succ endH startH :: NonEmpty StartHour
+-- StartHour 5 :| []
 wrappedRange
   :: ( Rewrapped a a
      , Rewrapped b b
@@ -159,18 +185,31 @@ wrappedRange con inc s e =
     next c =
       (c,  c ^. _Wrapped . to (haveNext . con . inc))
 
+-- | Try to read a @String@ value into a @Natural@ representing a whole hour,
+-- in 24 hour time. "1"  .. "23" or "00"
+--
+-- >>> strToHourNatural "1"
+-- Right 1
+-- >>> strToHourNatural "10"
+-- Right 10
+-- >>> strToHourNatural "0"
+-- Left "Unable to parse 0 into acceptable hour value. Expected '1'-'23' or '00'"
+-- >>> strToHourNatural  "00"
+-- Right 0
 strToHourNatural :: String -> Either String Natural
 strToHourNatural s = maybe err toNat $ readMaybe (trim s)
   where
     trim = let f = reverse . dropWhile (== ' ') in f . f
 
     err = Left ( "Unable to parse " <> s <>
-                 " into acceptable hour value. Between '1'-'23' or '00'"
+                 " into acceptable hour value. Expected '1'-'23' or '00'"
                )
     toNat n = if isHour n then Right (fromInteger n) else err
 
     isHour n = (n >= 1 && n <= 23) || (s == "00" && n == 0)
 
+-- | Convenience function for @wrappedRange@ that provides a list of @UTCTime@
+-- at hourly intervals between the given start and end times.
 utcRangeHours
   :: ( Rewrapped start start
      , Rewrapped end end
@@ -180,11 +219,23 @@ utcRangeHours
   => start
   -> end
   -> NonEmpty UTCTime
-utcRangeHours s e =
-  runIdentity <$> wrappedRange Identity (addUTCTime oneHour) s e
+utcRangeHours s e = runIdentity <$>
+  wrappedRange Identity (addUTCTime oneHour) s e
   where
     oneHour = 3600
 
+-- | Given two wrapped @Day@ values, provide a @NonEmpty@ list of all the days
+-- in between.
+-- >>> import Control.Lens
+-- >>> import Data.Time
+-- >>> :set -XFlexibleContexts
+-- >>> let startD = _Wrapped # (fromGregorian (2017 :: Integer) (2 :: Int) (20 :: Int)) :: StartDate
+-- >>> let endD = _Wrapped # (fromGregorian (2017 :: Integer) (2 :: Int) (27 :: Int)) :: EndDate
+
+-- >>> datesInRange startD endD
+-- DayInRange 2017-02-20 :| [DayInRange 2017-02-21,DayInRange 2017-02-22,DayInRange 2017-02-23,DayInRange 2017-02-24,DayInRange 2017-02-25,DayInRange 2017-02-26,DayInRange 2017-02-27]
+-- >>> datesInRange endD startD
+-- DayInRange 2017-02-27 :| []
 datesInRange
   :: ( Rewrapped a a
      , Rewrapped b b
@@ -193,8 +244,10 @@ datesInRange
      )
     => a -> b -> NonEmpty DayInRange
 datesInRange =
-  wrappedRange (^. from _Wrapped) (addDays 1)
+  wrappedRange (_Wrapped #) (addDays 1)
 
+-- | From the given starting points, construct a @Ranges@ record with ranges
+-- and bounds constructed. Not providing an @EndDate@ will use the @StartDate@.
 buildRanges
   :: StartDate
   -> Maybe EndDate
@@ -204,7 +257,7 @@ buildRanges
 buildRanges startD mayEndD startH endH =
   Ranges lower upper neDays utcz
   where
-    endD' = fromMaybe (startD ^. _Wrapped . to (^. from _Wrapped)) mayEndD
-    (lower, upper) = bounds startD endD' startH endH
+    endD' = fromMaybe (startD ^. _Wrapped . to (_Wrapped #)) mayEndD
+    (lower, upper) = boundFromWrapped startD endD' startH endH
     neDays = datesInRange startD endD'
     utcz = utcRangeHours lower upper
